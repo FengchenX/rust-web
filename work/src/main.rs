f30b940317d2068851aba0f0077b71c532f7b0c9
chenfeng <979924272@qq.com>
2019-11-29 09:50:28 +0800

    add

---
+++ work/src/main.rs
@@ -0,0 +1,10 @@
+
+pub mod movie;
+pub mod auth;
+
+use movie::{movie};
+
+fn main() {
+    movie();
+    println!("Hello, world!");
+}
---
+++ work/target/debug/deps/work-67d15d3912307905.d
@@ -0,0 +1,8 @@
+C:\Users\DELL\Desktop\workspace\rust-web\work\target\debug\deps\work-67d15d3912307905.exe: src\main.rs src\movie\mod.rs src\auth\mod.rs src\auth\util\mod.rs
+
+C:\Users\DELL\Desktop\workspace\rust-web\work\target\debug\deps\work-67d15d3912307905.d: src\main.rs src\movie\mod.rs src\auth\mod.rs src\auth\util\mod.rs
+
+src\main.rs:
+src\movie\mod.rs:
+src\auth\mod.rs:
+src\auth\util\mod.rs:
