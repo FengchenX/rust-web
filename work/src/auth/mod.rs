dc2ea8261e84fc7e193f2f55dee8fcb4b5ac4166
chenfeng <979924272@qq.com>
2019-12-02 18:31:22 +0800

    add

--- work/src/auth/mod.rs
+++ work/src/auth/mod.rs
@@ -1,6 +1,21 @@
+use actix_web::{get, App, HttpServer, Responder, web};
 
+
+
+#[get("/{id}/{name}/index.html")]
+fn index(info: web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.1, info.0)
+}
 
 pub fn run() {
-    println!("auth is running")
+    println!("auth is running");
 
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run()
+        .unwrap()
 }
